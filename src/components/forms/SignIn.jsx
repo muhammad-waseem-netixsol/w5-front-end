8e57da548b4d86c54a03c3df0997aa39839a3359
muhammad-waseem-netixsol <muhammad_waseem_intern@netixsol.com>
2024-01-12 02:56:32 +0500

    waseem

--- src/components/forms/SignIn.jsx
+++ src/components/forms/SignIn.jsx
@@ -1,83 +1,129 @@
-import {useState, useContext} from 'react';
-import {useNavigate} from "react-router-dom";
-import toast from 'react-hot-toast';
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 import "./Signin.css";
-import { AuthContext } from '../../context/context';
+import { AuthContext } from "../../context/context";
+import main_img from "../image/1.png";
+import { motion } from "framer-motion";
 
-
-  const SignIn = () => {
-    const auth = useContext(AuthContext);
-    const navigate = useNavigate();
-    const [user, setUser] = useState({
-        email:"",
-        password:""
+const SignIn = () => {
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const [emailValidity, setEmailValidity] = useState(false);
+  const [passwordValidity, setPasswordValidity] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUser({
+      ...user,
+      [name]: value,
     });
-    const [emailValidity, setEmailValidity] = useState(false);
-    const [passwordValidity, setPasswordValidity] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setUser({
-          ...user,
-          [name]: value,
-        });
-      };
-      const onLogUserIn = async event => {
-        event.preventDefault();
-        if(user.email.trim() === ""){
-          setEmailValidity(true);
-          toast.error("Email Can't Be Empty!")
-          return;
-        }
-        if(!user.email.trim().includes("@") || !user.email.trim().includes(".com") || !user.email.trim().length > 12){
-          setEmailValidity(true);
-          toast.error("Email is not Valid!")
-          return;
-        }
-        if(user.password.trim() === ""){
-          setPasswordValidity(true);
-          toast.error("Password can not be empty!");
-          return;
-        }
-        setLoading(true);
-        
-        const data = await fetch("https://week-5-task-backend.vercel.app/sign-in", {
-          method: "POST",
-          headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              email: user.email,
-              name:user.name,
-              password: user.password
-            })
-      });
-      
-      if(!data.ok){
-        setLoading(false);
-        const response = await data.json();
-        toast.error(response.message || "Error Occured!");
-        return;
-      }
+  };
+  const onLogUserIn = async (event) => {
+    event.preventDefault();
+    if (user.email.trim() === "") {
+      setEmailValidity(true);
+      toast.error("Email Can't Be Empty!");
+      return;
+    }
+    if (
+      !user.email.trim().includes("@") ||
+      !user.email.trim().includes(".com") ||
+      !user.email.trim().length > 12
+    ) {
+      setEmailValidity(true);
+      toast.error("Email is not Valid!");
+      return;
+    }
+    if (user.password.trim() === "") {
+      setPasswordValidity(true);
+      toast.error("Password can not be empty!");
+      return;
+    }
+    setLoading(true);
+
+    const data = await fetch("https://week-5-task-backend.vercel.app/sign-in", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: user.email,
+        name: user.name,
+        password: user.password,
+      }),
+    });
+
+    if (!data.ok) {
+      setLoading(false);
       const response = await data.json();
-        setLoading(false);
-        toast.success(response.message);
-        auth.login(response.userId,response.token );
-        return navigate("/");
-      }
-    return (
-        <div>
-          {loading && <div className='flex justify-center items-center overlay h-screen w-screen opacity-55 bg-black absolute left-0 top-0 cursor-wait'>
-          <div className="lds-ring"><div></div><div></div></div>
-          </div>}
-        <h1 className='text-center py-5'>Login</h1>
-       <form className='max-w-[500px] flex gap-3 flex-col mx-auto' onSubmit={onLogUserIn}>
-            <input className={`outline-none border py-2 px-2 block w-full ${emailValidity && "border-red-500"}`} type="text" placeholder={`${!emailValidity ? "Enter email here..." : "Email is compulsory..."}`} onChange={handleInputChange}  name='email'/>
-            <input className={`outline-none border py-2 px-2 block w-full ${passwordValidity && "border-red-500"}`} type="password" placeholder={`${!passwordValidity ? "Enter password here..." : "Password is compulsory..."}`} onChange={handleInputChange}  name='password'/>
-          <button className='border w-full py-2 cursor-pointer hover:bg-slate-800 bg-black text-white flex justify-center items-center gap-4' type="submit">SIGN IN</button>
-       </form>
+      toast.error(response.message || "Error Occured!");
+      return;
+    }
+    const response = await data.json();
+    setLoading(false);
+    toast.success(response.message);
+    auth.login(response.userId, response.token);
+    return navigate("/");
+  };
+  return (
+    <div className="md:grid md:grid-cols-2 h-full md:justify-center">
+      <div className="flex justify-center items-center">
+        {" "}
+        <img src={main_img} alt="" />
+      </div>
+
+      {loading && (
+        <div className="flex justify-center items-center overlay h-screen w-screen opacity-55 bg-black absolute left-0 top-0 cursor-wait">
+          <div className="lds-ring">
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      )}
+      <div className="md:flex md:justify-center md:items-center">
+        <form
+          className="sm:max-w-[500px] w-full flex gap-3 flex-col mx-auto"
+          onSubmit={onLogUserIn}
+        >
+          <input
+            className={`outline-none border py-2 px-2 block w-full ${
+              emailValidity && "border-red-500"
+            }`}
+            type="text"
+            placeholder={`${
+              !emailValidity ? "Enter email here..." : "Email is compulsory..."
+            }`}
+            onChange={handleInputChange}
+            name="email"
+          />
+          <input
+            className={`outline-none border py-2 px-2 block w-full ${
+              passwordValidity && "border-red-500"
+            }`}
+            type="password"
+            placeholder={`${
+              !passwordValidity
+                ? "Enter password here..."
+                : "Password is compulsory..."
+            }`}
+            onChange={handleInputChange}
+            name="password"
+          />
+          <button
+            className="border w-full py-2 cursor-pointer hover:bg-slate-800 bg-black text-white flex justify-center items-center gap-4"
+            type="submit"
+          >
+            SIGN IN
+          </button>
+        </form>
+      </div>
     </div>
-    );
-}
+  );
+};
 
 export default SignIn;
--- src/components/forms/SignUp.jsx
+++ src/components/forms/SignUp.jsx
@@ -2,7 +2,7 @@ import {useState} from 'react';
 import {useNavigate} from "react-router-dom";
 import toast from "react-hot-toast";
 import "./Signup.css";
-
+import main_img from "../image/1.png"
 
 const SignUp = () => {
     const navigate = useNavigate();
@@ -81,17 +81,22 @@ const SignUp = () => {
           return navigate("/login");
       }
     return (
-        <div className=''>
+        <div className='md:grid flex flex-col-reverse md:grid-cols-2 h-full md:justify-center'>
           {loading && <div className='flex justify-center items-center overlay h-screen w-screen opacity-55 bg-black absolute left-0 top-0 cursor-wait'>
           <div className="lds-ripple"><div></div><div></div></div>
           </div>}
-        <h1 className='text-center py-5'>Sign Up</h1>
-       <form className='max-w-[500px] flex gap-3 flex-col mx-auto' onSubmit={onCreateNewUser}>
+          <div className='md:flex md:justify-center md:items-center'>
+       <form className='sm:max-w-[500px] w-full flex gap-3 flex-col mx-auto' onSubmit={onCreateNewUser}>
             <input className={`outline-none border py-2 px-2 block w-full ${nameValidity && "border-red-500"}`} type="text" placeholder={`${!nameValidity ? "Enter name here..." : "Name is compulsory..."}`} onChange={handleInputChange}  name='name'/>
             <input className={`outline-none border py-2 px-2 block w-full ${emailValidity && "border-red-500"}`} type="text" placeholder={`${!emailValidity ? "Enter email here..." : "Email is compulsory..."}`} onChange={handleInputChange}  name='email'/>
             <input className={`outline-none border py-2 px-2 block w-full ${passwordValidity && "border-red-500"}`} type="password" placeholder={`${!passwordValidity ? "Enter password here..." : "Password is compulsory..."}`} onChange={handleInputChange}  name='password'/>
             <button  className='border w-full py-2 cursor-pointer hover:bg-slate-800 bg-black text-white flex justify-center items-center gap-4' type="submit">SIGN UP</button>
        </form>
+       </div>
+       <div className="flex justify-center items-center">
+        {" "}
+        <img src={main_img} alt="" />
+      </div>
     </div>
     );
 }
